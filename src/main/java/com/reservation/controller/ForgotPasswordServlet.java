698d171f9653fa52f22c43939075c580833ee1fa
IT25103902 <it25103902@my.sliit.lk>
2026-05-15 12:40:26 +0530

    Created All Classes in Controller pkg.

---
+++ src/main/java/com/reservation/controller/ForgotPasswordServlet.java
@@ -0,0 +1,6 @@
+package com.reservation.controller;
+
+public class ForgotPasswordServlet {
+
+    // forgot password servlet
+}
---
+++ src/main/java/com/reservation/controller/ResetPasswordServlet.java
@@ -0,0 +1,6 @@
+package com.reservation.controller;
+
+public class ResetPasswordServlet {
+
+    // reset password servlet
+}

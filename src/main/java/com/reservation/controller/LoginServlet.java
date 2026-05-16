8ff6cf9df2e49954c1481567bf0503846290d3ba
IT25103902 <it25103902@my.sliit.lk>
2026-05-16 15:22:35 +0530

    Updated Login Servlet file.

--- src/main/java/com/reservation/controller/LoginServlet.java
+++ src/main/java/com/reservation/controller/LoginServlet.java
@@ -1,6 +1,39 @@
 package com.reservation.controller;
 
-public class LoginServlet {
+import com.reservation.service.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
 
-    //Login servlet
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        HttpSession session = request.getSession();
+        UserService userService = new UserService();
+
+        String role = userService.loginUser(email, password);
+
+        if (role != null) {
+            session.setAttribute("userEmail", email);
+            session.setAttribute("role", role);
+
+            if ("admin".equalsIgnoreCase(role)) {
+                response.sendRedirect("admin_dashboard.jsp");
+            } else {
+                response.sendRedirect("dashboard.jsp");
+            }
+
+        } else {
+            response.sendRedirect("login.jsp?error=invalid");
+        }
+    }
 }
